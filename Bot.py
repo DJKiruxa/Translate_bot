1b7c9618beebcc5d65472d03e7a3ffecd2944e3c
DJKiruxa <40432425+DJKiruxa@users.noreply.github.com>
2019-04-25 23:15:48 +0300

    Translate_Bot v0.5
    Game was added

--- Bot.py
+++ Bot.py
@@ -2,13 +2,16 @@
 from telegram.ext import Updater, MessageHandler, Filters, ConversationHandler, CommandHandler
 from telegram import ReplyKeyboardMarkup
 from MainClass import TranslatorClass as tcs
+from MainClass import GameClass
 
-
+Gc = GameClass()
+#создание клавиатуры
 reply_keyboard = [['/help', '/stop'],
-                  ['/startranslate']]
+                  ['/startranslate', '/letsplay']]
 markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
 # Определяем функцию-обработчик сообщений.
 # У неё два параметра, сам бот и класс updater, принявший сообщение.
+
 def start_bot(bot, update):
     #функия привествия
     update.message.reply_text("О здравствуй мой повелитель, узнай как управлять мною используя команду /help", reply_markup = markup)
@@ -23,12 +26,30 @@ def translate(bot, update):
     #отсылается переведенное сообщение
     update.message.reply_text(tcs.text_to_text(update.message.text), reply_markup = markup)
 
+def letsplay(bot, update):
+    #начало игры
+    update.message.reply_text("Игра началась! Попробуйте угадать перевод слова от яндекса!\n"
+        "Врядли у вас получится (У Yandex(a) косячный перевод :0)\n"
+        "Когда будете готовы, напишите 'Готов'  ")
+    return 2
+
+def game1(bot, update):
+    global Gc
+    #сама игра
+    update.message.reply_text(Gc.start_game())
+    return 3
+
+def game2(bot,update):
+    global Gc
+    update.message.reply_text(Gc.checker(update.message.text))
+    return 2
 def help(bot, update):
-    #команда показывающая все возможные команды
+    #функция показывающая все возможные команды
     update.message.reply_text("Вот все мои команды:\n"
         "/help - все команды\n"
         "/stop - прекратить выполнение команды\n"
-        "/startranslate - начать перевод", reply_markup=markup)
+        "/startranslate - начать перевод\n"
+        "/letsplay - начало игры", reply_markup=markup)
     return ConversationHandler.END
 
 def stop(bot, update):
@@ -47,10 +68,13 @@ def main():
     conv_handler = ConversationHandler(
     entry_points=[CommandHandler('startranslate', start_translate),
         CommandHandler('help', help),
-        CommandHandler('start', start_bot)
+        CommandHandler('start', start_bot),
+        CommandHandler('letsplay', letsplay)
         ],
         states={
             1:[MessageHandler(Filters.text, translate)],
+            2:[MessageHandler(Filters.text, game1)],
+            3:[MessageHandler(Filters.text, game2)]
             },
         fallbacks=[CommandHandler('stop', stop)])
 
--- MainClass.py
+++ MainClass.py
@@ -1,59 +1,81 @@
-from translator_init import translate1
-from speechToText import *
-from textToSpeech import *
+from string import ascii_letters
 from gtts import gTTS
 from yandex_translate import YandexTranslate
-import speech_recognition as sr  # РРјРїРѕСЂС‚РёСЂСѓРµРј СЂР°СЃРїРѕР·РЅР°РІР°РЅРёРµ РіРѕР»РѕСЃР°
-# РњРѕРґСѓР»СЊ РґР»СЏ СѓРґРѕР±РЅРѕРіРѕ РёСЃРїРѕР»СЊР·РѕРІР°РЅРёСЏ
+import speech_recognition as sr  # Импортируем распознавание голоса
+import random
+# Модуль для удобного использования
 
-#API KEY FOR YNDX TRANSLATE
+# API KEY FOR YNDX TRANSLATE
 keylong = '4a360.907b12c1d0b00c3d0555378e945df82d09824dd2'
 translate1 = YandexTranslate('trnsl.1.1.20181216T165708Z.256f0281500' + keylong)
-#API KEY FOR YNDX TRANSLATE
+# API KEY FOR YNDX TRANSLATE
+
 
 class TranslatorClass:
     def text_to_text(text, lang_from='ru', lang_to='en'):
         # Translator. First elemant - text
         # Second element - language from which to translate
         # Third - language to translate
-        if lang_to == 'en':
-            lang_to_y = 'en'
-        elif lang_to == 'ru':
-            lang_to_y = 'ru'
-        if lang_from == 'en':
-            lang_fy = 'en'
-        elif lang_from == 'ru':
-            lang_fy = 'ru'
-        return(str((translate1.translate(text, lang_fy + '-' + lang_to_y).get('text')[0])))
+        if all(map(lambda c: c in ascii_letters, text)) is True:
+            lang_from = 'en'
+            lang_to = 'ru'
+        else:
+            lang_from = 'ru'
+            lang_to = 'en'
+        return(str((translate1.translate(text, lang_from + '-' + lang_to).get('text')[0])))
 
     def stt(flag, path='', lang='ru'):
-        r = sr.Recognizer()  # РРЅРёС†РёР°Р»РёР·РёСЂСѓРµРј СЂР°СЃРїРѕР·РЅР°РІР°С‚РµР»СЊ РіРѕР»РѕСЃР°
-        if flag is False:  # РџСЂРѕРІРµСЂРєР° С„Р»Р°РіР°
+        r = sr.Recognizer()  # Инициализируем распознаватель голоса
+        if flag is False:  # Проверка флага
             with sr.Microphone() as source:
-                audio = r.listen(source)  # РџСЂРѕСЃР»СѓС€РёРІР°РµРј РјРёРєСЂРѕС„РѕРЅ
+                audio = r.listen(source)  # Прослушиваем микрофон
         else:
-            open_file = sr.AudioFile(path)  # РћС‚РєСЂС‹РІР°РµРј С„Р°Р№Р»
+            open_file = sr.AudioFile(path)  # Открываем файл
             audio = r.record(open_file)
         try:
-            lng = ''  # РџР°СЂР°РјРµС‚СЂ СЏР·С‹РєР° РґР»СЏ google
+            lng = ''  # Параметр языка для google
             if lang == 'ru':
-                lng = 'ru-RU'  # Р СѓСЃСЃРєРёР№ СЏР·С‹Рє
+                lng = 'ru-RU'  # Русский язык
             elif lang == 'en':
-                lng = 'en-EN'  # РђРЅРіР»РёР№СЃРєРёР№ СЏР·С‹Рє
+                lng = 'en-EN'  # Английский язык
             return(r.recognize_google(audio, language=lng))
-            # Р’РѕР·РІСЂР°С‰Р°РµРј СЂР°СЃРїРѕР·РЅР°РЅРЅС‹Р№ С‚РµРєСЃС‚
-        except sr.UnknownValueError:  # РћС€РёР±РєР° СЂР°СЃРїРѕР·РЅР°РІР°РЅРёСЏ
+            # Возвращаем распознанный текст
+        except sr.UnknownValueError:  # Ошибка распознавания
             pass
-        except sr.RequestError as e:  # РћС€РёР±РєР° Р·Р°РїСЂРѕСЃР° Рє СЃРµСЂРІРёСЃСѓ
+        except sr.RequestError as e:  # Ошибка запроса к сервису
             pass
 
     def tts(text1, fl, lang_to='en'):
-        # РџРµСЂРІС‹Р№ СЌР»РµРјРµРЅС‚ С‚РµРєСЃС‚, РІС‚РѕСЂРѕР№ - СЏР·С‹Рє РґР»СЏ СЃС‡РёС‚С‹РІР°РЅРёСЏ
+        # Первый элемент текст, второй - язык для считывания
         if lang_to == 'ru':
             tts = gTTS(text=text1, lang='ru')
             tts.save(fl)
         if lang_to == 'en':
             tts = gTTS(text=text1, lang='en')
-            tts.save(fl)
+            return tts
 
-        
+line = None
+class GameClass:
+    def __init__(self):
+        self.score = 0
+
+    def start_game(self):
+        global line
+        line = random.choice(open('words_alpha.txt').readlines())[:-1]
+        while all(map(lambda c: c in ascii_letters, \
+                      TranslatorClass.text_to_text(\
+                                                   line, \
+                                                   'en', 'ru'))) is True:
+            line = random.choice(open('words_alpha.txt').readlines())[:-1]
+
+        return(line)
+
+    def checker(self, inp):
+        global line
+        if TranslatorClass.text_to_text(line, 'en', \
+                                        'ru').lower()[:-1] == inp.lower()[:-1]:
+            self.score += 1
+            return('Правильно. Ваш счёт: {}.'.format(self.score))
+        else:
+            return('Неверно, правильный ответ: {}. Ваш счёт: {}.'.format(TranslatorClass.text_to_text(line, \
+                                                                                                      'en', 'ru'), self.score))
